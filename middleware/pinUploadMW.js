947ce08d1d039bd415e6e9501b2235032135cb3e
Vladimir Drugov <wolffdrugov@yandex.ru>
2022-06-28 15:24:48 +0300

    added mkdir

--- middleware/pinUploadMW.js
+++ middleware/pinUploadMW.js
@@ -25,14 +25,22 @@ module.exports = async function (pin) {
 			keywords: tags,
 		});
 	const directory = 'images';
-	fs.readdir(directory, (err, files) => {
-		if (err) throw err;
-
-		for (const file of files) {
-			fs.unlink(path.join(directory, file), (err) => {
-				if (err) throw err;
-			});
+	try {
+		if (!fs.existsSync(directory)) {
+			fs.mkdirSync(directory);
 		}
-	});
+		fs.readdir(directory, (err, files) => {
+			if (err) throw err;
+
+			for (const file of files) {
+				fs.unlink(path.join(directory, file), (err) => {
+					if (err) throw err;
+				});
+			}
+		});
+	} catch (e) {
+		console.log(e);
+	}
+
 	return tags;
 };
