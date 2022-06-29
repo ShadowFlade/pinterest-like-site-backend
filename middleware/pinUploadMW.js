e9067334247c3dbc31c787dd9fa0ee96d879fdd8
Vladimir Drugov <wolffdrugov@yandex.ru>
2022-06-29 15:34:55 +0300

    updates

--- middleware/pinUploadMW.js
+++ middleware/pinUploadMW.js
@@ -4,30 +4,31 @@ const { ObjectId } = require('mongodb');
 const fs = require('fs');
 const path = require('path');
 module.exports = async function (pin) {
-	const URL = pin.file.path || pin.URL;
-	console.log('🚀 ~ file: pinUploadMW.js ~ line 8 ~ URL', URL);
-
-	const uploadedImg = await upload({
-		img: URL,
-		id: `${nanoid()}`,
-	});
-	const publicURL = uploadedImg.url.replace(/http/, 'https');
-	const tags = [];
-	const tagsData = uploadedImg.info.detection.object_detection.data['cld-fashion'].tags;
-	tags.push(...Object.keys(tagsData));
-
-	await client
-		.db()
-		.collection('pins')
-		.insertOne({
-			img: publicURL,
-			title: pin.title,
-			description: pin.description || '',
-			authorId: ObjectId(pin.authorId),
-			keywords: tags,
-		});
-	const directory = path.resolve(__dirname, 'images');
 	try {
+		const URL = pin.file.path || pin.URL;
+		console.log('🚀 ~ file: pinUploadMW.js ~ line 8 ~ URL', URL);
+
+		const uploadedImg = await upload({
+			img: URL,
+			id: `${nanoid()}`,
+		});
+		const publicURL = uploadedImg.url.replace(/http/, 'https');
+		const tags = [];
+		const tagsData = uploadedImg.info.detection.object_detection.data['cld-fashion'].tags;
+		tags.push(...Object.keys(tagsData));
+
+		await client
+			.db()
+			.collection('pins')
+			.insertOne({
+				img: publicURL,
+				title: pin.title,
+				description: pin.description || '',
+				authorId: ObjectId(pin.authorId),
+				keywords: tags,
+			});
+		const directory = path.resolve(__dirname, 'images');
+
 		const files = fs.readdirSync(directory);
 		files.forEach((file) => {
 			fs.unlink(path.resolve(directory, file), (err) => {
