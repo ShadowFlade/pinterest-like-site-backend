512d285480e78aabd4ec0adb6da3e2faf2fb10eb
shadowflade <wolffdrugov@yandex.ru>
2023-10-08 21:49:04 +0300

    master:added add field one_timer with cli args

--- api/Postponed.js
+++ api/Postponed.js
@@ -3,12 +3,11 @@ const { client } = require('../middleware/connectionMW');
 class Postponed {
 	constructor() {}
 	//get pins which should be posted this minute
-	// async getPins() {
-	// 	const pinsCollection = await client.db().collection('pins');
-	// 	pinsCollection.aggregate([
-	// 		$match:{
-
-	// 		}
-	// 	])
-	// }
+	ONE_DAY_IN_MILSECONDS = 60 * 60 * 24 * 1000;
+	async getPins() {
+		const pinsCollection = await client.db().collection('pins');
+		const dayStart = Date.now();
+		const dayEnd = this.ONE_DAY_IN_MILI_SECONDS + now;
+		pinsCollection.find({ DATE_TO_PUBLISH: [{ $gte: dayStart }, { $lt: dayEnd }] });
+	}
 }
--- one_timers/addField.js
+++ one_timers/addField.js
@@ -2,18 +2,22 @@ require('dotenv').config();
 const { client } = require('../middleware/connectionMW');
 
 async function run() {
-	const FIELD = 'DATE_INSERT';
+	let FIELD = 'DATE_TO_PUBLISH';
+	if (process.argv.length > 2) {
+		FIELD = process.argv[2];
+	}
 	await client.connect();
 	const pins = await client.db('pinterest').collection('pins');
 	const pin = await pins.findOne({});
-	if (!pin.hasOwnProperty(FIELD)) {
+	if (pin.hasOwnProperty(FIELD)) {
 		process.exit();
 	}
-	const updateResult = pins.updateMany(
+	const updateResult = await pins.updateMany(
 		{},
 		{ $set: { [FIELD]: null } },
 		{ upsert: false, multi: true }
 	);
-	return updateResult;
+	console.log(updateResult, ' update result');
+	process.exit();
 }
 run();
